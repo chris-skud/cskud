6a3d54188f7670e9031c0179b805021d70bb6aeb
Chris Skudlarczyk <chris.skud@gmail.com>
2012-09-12 12:50:38 -0600

    Added Impress subsite

--- main.py
+++ main.py
@@ -10,10 +10,14 @@ from google.appengine.ext.webapp import template
 class MainHandler(webapp.RequestHandler):
     def get(self):
         self.response.out.write(template.render('main.html', {}))
+
+class ProtoHandler(webapp.RequestHandler):
+    def get(self):
+        self.response.out.write(template.render('proto/main.html', {}))
         
 def main():
     
-    application = webapp.WSGIApplication([('/', MainHandler)],debug=True)
+    application = webapp.WSGIApplication([('/', MainHandler), ('/proto/', ProtoHandler)],debug=True)
     util.run_wsgi_app(application)
     
 if __name__ == '__main__':
