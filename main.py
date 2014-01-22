240de11560149d95acfc30813f088564c3619c42
Chris Skudlarczyk <christopher.skudlarczyk@pearson.com>
2014-01-22 12:39:31 -0700

    Some responsive updates

--- main.py
+++ main.py
@@ -1,29 +1,24 @@
 #!/usr/bin/env python
 import cgi
 import wsgiref.handlers
+import json
 from google.appengine.ext import webapp
 from google.appengine.api import users
 from google.appengine.ext.webapp import util
 from google.appengine.ext.webapp import template
-
+from google.appengine.api import urlfetch
 
 class MainHandler(webapp.RequestHandler):
-    def get(self):
-        self.response.out.write(template.render('main.html', {}))
+  def get(self):
+    self.response.out.write(template.render('main.html', {}))
 
 class ProtoHandler(webapp.RequestHandler):
-    def get(self):
-        self.response.out.write(template.render('proto/main.html', {}))
+  def get(self):
+    self.response.out.write(template.render('proto/main.html', {}))
 
-class ClaytonHandler(webapp.RequestHandler):
-    def get(self):
-        self.response.out.write(template.render('clayton.html', {}))
-        
 def main():
-    
-    application = webapp.WSGIApplication([('/', MainHandler), ('/proto/', ProtoHandler), ('/clayton', ClaytonHandler)], debug=True)
-    util.run_wsgi_app(application)
-    
-if __name__ == '__main__':
-    main()
+  application = webapp.WSGIApplication([('/', MainHandler), ('/proto/', ProtoHandler)], debug=True)
+  util.run_wsgi_app(application)
 
+if __name__ == '__main__':
+  main()
